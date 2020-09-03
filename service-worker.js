55ae15ae247906a0c76f1cdeb91a6f4962bfb48a
Anatasia <krajnova.stasy@gmail.com>
2020-09-03 22:25:27 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.1e108b11d8d7a11c72e0ba31c943624e.js"
 );
 
 self.addEventListener('message', (event) => {
